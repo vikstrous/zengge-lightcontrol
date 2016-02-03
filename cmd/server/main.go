d3609a9b5a6eecb721125b8eeb30b44bce93e276
Viktor Stanchev <me@viktorstanchev.com>
2016-02-02 21:18:49 -0800

    set color

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,41 +8,42 @@ import (
 	"github.com/vikstrous/zengge-lightcontrol/local"
 )
 
-func handlerOn(w http.ResponseWriter, r *http.Request) {
-	transport, err := local.NewTransport("home.viktorstanchev.com:5577")
+var transport *local.Transport
+var controller *control.Controller
+
+func init() {
+	var err error
+	transport, err = local.NewTransport("home.viktorstanchev.com:5577")
 	if err != nil {
 		fmt.Printf("Failed to connect. %s", err)
+		return
 	}
-	controller := &control.Controller{transport}
-	err = controller.SetPower(true)
+	controller = &control.Controller{transport}
+}
+
+func handlerOn(w http.ResponseWriter, r *http.Request) {
+	err := controller.SetPower(true)
 	if err != nil {
 		fmt.Printf("Failed to set power. %s", err)
+		return
 	}
 	w.Write([]byte("on"))
 }
 
 func handlerOff(w http.ResponseWriter, r *http.Request) {
-	transport, err := local.NewTransport("home.viktorstanchev.com:5577")
-	if err != nil {
-		fmt.Printf("Failed to connect. %s", err)
-	}
-	controller := &control.Controller{transport}
-	err = controller.SetPower(false)
+	err := controller.SetPower(false)
 	if err != nil {
 		fmt.Printf("Failed to set power. %s", err)
+		return
 	}
 	w.Write([]byte("off"))
 }
 
 func handlerState(w http.ResponseWriter, r *http.Request) {
-	transport, err := local.NewTransport("home.viktorstanchev.com:5577")
-	if err != nil {
-		fmt.Printf("Failed to connect. %s", err)
-	}
-	controller := &control.Controller{transport}
 	state, err := controller.GetState()
 	if err != nil {
 		fmt.Printf("Failed to get state. %s", err)
+		return
 	}
 	if !state.IsOn {
 		w.Write([]byte(fmt.Sprintf("Off")))
@@ -51,10 +52,28 @@ func handlerState(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerColor(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	colorStr := r.FormValue("color")
+	color := control.ParseColorString(colorStr)
+	if color == nil {
+		fmt.Printf("Failed to parse color.")
+		return
+	}
+
+	err := controller.SetColor(*color)
+	if err != nil {
+		fmt.Printf("Failed to set color. %s", err)
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("done")))
+}
+
 func main() {
 	http.HandleFunc("/on", handlerOn)
 	http.HandleFunc("/off", handlerOff)
 	http.HandleFunc("/state", handlerState)
+	http.HandleFunc("/color", handlerColor)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":8099", nil)
 }
